164546de992d2e61e46debcee60d1470a1cb76ca
Yuvraj1239 <120008947+Yuvraj1239@users.noreply.github.com>
2023-10-25 04:56:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import requests
-import pyttsx3
+# import pyttsx3
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
     data = response.json()
@@ -26,22 +26,22 @@ similarity = pickle.load(open('similarity.pkl','rb'))
 
 
 
-def speak(text):
-    # Initialize the text-to-speech engine
+# def speak(text):
+#     # Initialize the text-to-speech engine
 
-    engine = pyttsx3.init()
+#     engine = pyttsx3.init()
 
-    # Set properties (optional)
-    engine.setProperty('rate', 150)  # Speed of speech (words per minute)
-    engine.setProperty('volume', 1)  # Volume (0.0 to 1.0)
+#     # Set properties (optional)
+#     engine.setProperty('rate', 150)  # Speed of speech (words per minute)
+#     engine.setProperty('volume', 1)  # Volume (0.0 to 1.0)
 
-    # Convert and speak the text
-    engine.say(text)
+#     # Convert and speak the text
+#     engine.say(text)
 
-    # Wait for the speech to finish
-    engine.runAndWait()
-    return
-    exit(0)
+#     # Wait for the speech to finish
+#     engine.runAndWait()
+#     return
+#     exit(0)
 
 
 # Example usage
@@ -57,22 +57,22 @@ if st.button('Recommend'):
     names,posters = recommend(selected_movie_name)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        speak(names[0])
+        # speak(names[0])
         st.text(names[0])
         st.image(posters[0])
     with col2:
-        speak(names[1])
+        # speak(names[1])
         st.text(names[1])
         st.image(posters[1])
     with col3:
-        speak(names[2])
+        # speak(names[2])
         st.text(names[2])
         st.image(posters[2])
     with col4:
-        speak(names[3])
+        # speak(names[3])
         st.text(names[3])
         st.image(posters[3])
     with col5:
-        speak(names[4])
+        # speak(names[4])
         st.text(names[4])
         st.image(posters[4])
